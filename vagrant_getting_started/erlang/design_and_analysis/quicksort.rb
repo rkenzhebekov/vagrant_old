78618bed62add0ee6d5f2f64ed79713f50866885
Ruslan Kenzhebekov <rkenzheb@pincsolutions.com>
2015-11-01 12:14:23 -0800

    Implement quicksort with total comparison

--- vagrant_getting_started/erlang/design_and_analysis/quicksort.rb
+++ vagrant_getting_started/erlang/design_and_analysis/quicksort.rb
@@ -4,34 +4,44 @@ while (line = file.gets)
   arr << line.to_i
 end
 file.close
-# arr = [1,3,5,2,4,6]
 
-def quicksort(arr, low, high)
-  if low < high
-    p = partition(arr, low, high)
-    quicksort(arr, low, p - 1)
-    quicksort(arr, p + 1, high)
+class Sorting
+  attr_accessor :counter
+  def initialize
+    @counter = 0
   end
-end
 
-def partition(arr, low, high)
-  pivot = arr[low]
-  i = low + 1
-  for j in low + 1 .. high
-    if arr[j] < pivot
-      swap(arr, i, j)
-      i += 1
+  def quicksort(arr, low, high)
+    if low < high
+      p = partition(arr, low, high)
+      quicksort(arr, low, p - 1)
+      quicksort(arr, p + 1, high)
     end
   end
-  swap(arr, low, i - 1)
-  i - 1
+
+  def partition(arr, low, high)
+    pivot = arr[low]
+    i = low + 1
+    for j in low + 1 .. high
+      @counter += 1
+      if arr[j] < pivot
+        swap(arr, i, j)
+        i += 1
+      end
+    end
+    swap(arr, low, i - 1)
+    i - 1
+  end
+
+  def swap(arr, i, j)
+    temp = arr[j]
+    arr[j] = arr[i]
+    arr[i] = temp
+  end
 end
 
-def swap(arr, i, j)
-  temp = arr[j]
-  arr[j] = arr[i]
-  arr[i] = temp
-end
-
-quicksort(arr, 0, arr.length - 1)
+arr = [3, 9, 8, 4, 6, 10, 2, 5, 7, 1]
+s = Sorting.new
+s.quicksort(arr, 0, arr.length - 1)
+p s.counter
 p arr
